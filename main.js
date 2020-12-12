d41d81550076e1b2cb1b0ec403ba8d9d39c23298
Kwateru <33171676+Kwateru@users.noreply.github.com>
2020-12-12 17:21:48 +0900

    行頭と行末に""を追加
    splitで配列に格納して、forで各行に処理する

--- main.js
+++ main.js
@@ -8,14 +8,21 @@ $(function(){
 
         var inputVal = input.val();
 
-        var str = inputVal.replace(/\r?\n/g, ' OR ');
+        // textareaの内容を改行で分割して配列に格納
+        var inputValArray = inputVal.split('\n');
+
+        // 各行に対して処理する
+        var display = "";
+        for(i = 0; i < inputValArray.length; i++){
+            display += '"' + inputValArray[i] + '"' + "\n";
+        }
+
+        // 改行を「 OR 」に置換する
+        var str = display.replace(/\r?\n/g, ' OR ')
         result.val('refnum:(' + str + ')');
 
         // copyする
         result.select();
     })
-
-
-
 
 });
